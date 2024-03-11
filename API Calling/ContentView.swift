c08c6be9fba9a0400acde26d6bf76d1d58a38f50
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-03-11 09:33:15 -0500

    Initial Commit

---
+++ API Calling/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  API Calling
+//
+//  Created by Phillip Mantatsky on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
